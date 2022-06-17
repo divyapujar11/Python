195ee72aa04b89ac5fee9fffded3a55a3ceab43b
Divya Pujar <99553089+divyapujar11@users.noreply.github.com>
2022-06-17 14:31:59 +0530

    swap 2 numbers
    python program of swapping 2 numbers without third variable

---
+++ Swap2Num.py
@@ -0,0 +1,12 @@
+
+a=(int(input("Enter a value")))
+b=(int(input("enter b value")))
+
+print("A and B values before swapping:")
+print("a = ",a)
+print("b = ",b)
+
+a,b = b,a
+print("A and B values after swapping:")
+print("a = ",a)
+print("b = ",b)
