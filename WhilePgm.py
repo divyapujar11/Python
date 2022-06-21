759f7dff8cc1a92ab4da649d1deb02470e9dd011
Divya Pujar <99553089+divyapujar11@users.noreply.github.com>
2022-06-21 20:14:17 +0530

    Add files via upload

---
+++ WhilePgm.py
@@ -0,0 +1,13 @@
+# print patter using while
+# Divya Rocks Rocks Rocks 
+i=5
+while i<=5:
+    print("Divya ",end="")
+    j=1
+    while j<=4:
+        print("Rocks ",end="")
+        j=j+1
+
+    i=i+1
+    print()
+    print()
